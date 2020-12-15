609f58c6f3766f876049f4b5d9254e506ff294e5
luddwichr <luddwichr@users.noreply.github.com>
2020-12-15 14:16:51 +0100

    upgraded spring boot, spring cloud and migrate junit 4 to 5

--- src/test/java/com/github/hystrixplayground/MyHystrixCommandTest.java
+++ src/test/java/com/github/hystrixplayground/MyHystrixCommandTest.java
@@ -4,9 +4,7 @@ import com.github.hystrixplayground.MyHystrixCommand.MyAction;
 import com.netflix.config.ConfigurationManager;
 import com.netflix.hystrix.exception.HystrixBadRequestException;
 import com.netflix.hystrix.exception.HystrixRuntimeException;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.concurrent.*;
@@ -14,74 +12,68 @@ import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class MyHystrixCommandTest {
+class MyHystrixCommandTest {
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+	@Test
+	void successful() {
+		assertThat(new MyHystrixCommand(MyAction.SUCCESS).execute()).isEqualTo("Successful call");
+	}
 
-    @Test
-    public void successful() {
-        assertThat(new MyHystrixCommand(MyAction.SUCCESS).execute()).isEqualTo("Successful call");
-    }
+	@Test
+	void timeout() {
+		Integer originalTimeoutInMillis = ConfigurationManager.getConfigInstance()
+				.getInteger("hystrix.command.MyHystrixCommand.execution.isolation.thread.timeoutInMilliseconds", 1000);
+		ConfigurationManager.getConfigInstance()
+				.setProperty("hystrix.command.MyHystrixCommand.execution.isolation.thread.timeoutInMilliseconds", 10);
 
-    @Test
-    public void timeout() {
-        expectedException.expect(HystrixRuntimeException.class);
-        expectedException.expectCause(instanceOf(TimeoutException.class));
-        Integer originalTimeoutInMillis = ConfigurationManager.getConfigInstance()
-                .getInteger("hystrix.command.MyHystrixCommand.execution.isolation.thread.timeoutInMilliseconds", 1000);
-        ConfigurationManager.getConfigInstance()
-                .setProperty("hystrix.command.MyHystrixCommand.execution.isolation.thread.timeoutInMilliseconds", 10);
-        try {
-            new MyHystrixCommand(MyAction.TIMEOUT).execute();
-        } finally {
-            ConfigurationManager.getConfigInstance()
-                    .setProperty("hystrix.command.MyHystrixCommand.execution.isolation.thread.timeoutInMilliseconds", originalTimeoutInMillis);
-        }
-    }
+		assertThatExceptionOfType(HystrixRuntimeException.class)
+				.isThrownBy(() -> new MyHystrixCommand(MyAction.TIMEOUT).execute())
+				.withCauseInstanceOf(TimeoutException.class);
 
-    @Test
-    public void failure() {
-        expectedException.expect(HystrixRuntimeException.class);
-        expectedException.expectCause(instanceOf(IllegalStateException.class));
-        MyHystrixCommand command = new MyHystrixCommand(MyAction.FAILURE);
+		ConfigurationManager.getConfigInstance()
+				.setProperty("hystrix.command.MyHystrixCommand.execution.isolation.thread.timeoutInMilliseconds", originalTimeoutInMillis);
+	}
 
-        command.execute();
+	@Test
+	void failure() {
+		MyHystrixCommand command = new MyHystrixCommand(MyAction.FAILURE);
 
-        assertTrue(command.isFailedExecution());
-    }
+		assertThatExceptionOfType(HystrixRuntimeException.class)
+				.isThrownBy(command::execute)
+				.withCauseInstanceOf(IllegalStateException.class);
 
-    @Test
-    public void badRequest() {
-        expectedException.expect(HystrixBadRequestException.class);
-        expectedException.expectMessage("Bad request");
-        expectedException.expectCause(instanceOf(IllegalArgumentException.class));
-        MyHystrixCommand command = new MyHystrixCommand(MyAction.BAD_REQUEST);
+		assertThat(command.isFailedExecution()).isTrue();
+	}
 
-        command.execute();
-    }
+	@Test
+	void badRequest() {
+		MyHystrixCommand command = new MyHystrixCommand(MyAction.BAD_REQUEST);
 
-    @Test
-    public void threadpoolExhaustion() throws InterruptedException, ExecutionException {
-        Integer originalThreadpoolSize = ConfigurationManager.getConfigInstance().getInteger("hystrix.threadpool.default.coreSize", 10);
-        ConfigurationManager.getConfigInstance().setProperty("hystrix.threadpool.default.coreSize", 1);
-        expectedException.expect(ExecutionException.class);
-        expectedException.expectCause(instanceOf(HystrixRuntimeException.class));
+		assertThatExceptionOfType(HystrixBadRequestException.class)
+				.isThrownBy(command::execute)
+				.withCauseInstanceOf(IllegalArgumentException.class)
+				.withMessage("Bad request");
+	}
 
-        try {
-            ExecutorService executor = Executors.newFixedThreadPool(2);
-            List<Future<String>> futures = executor.invokeAll(IntStream.rangeClosed(1, 2)
-                    .<Callable<String>>mapToObj(i -> () -> new MyHystrixCommand(MyAction.SUCCESS_DELAYED).execute()).collect(toList()));
-            for (Future<String> future : futures) {
-                future.get();
-            }
-            executor.shutdown();
-            executor.awaitTermination(1, TimeUnit.SECONDS);
-        } finally {
-            ConfigurationManager.getConfigInstance().setProperty("hystrix.threadpool.default.coreSize", originalThreadpoolSize);
-        }
-    }
+	@Test
+	void threadpoolExhaustion() {
+		Integer originalThreadpoolSize = ConfigurationManager.getConfigInstance().getInteger("hystrix.threadpool.default.coreSize", 10);
+		ConfigurationManager.getConfigInstance().setProperty("hystrix.threadpool.default.coreSize", 1);
+
+		assertThatExceptionOfType(ExecutionException.class)
+				.isThrownBy(() -> {
+					ExecutorService executor = Executors.newFixedThreadPool(2);
+					List<Future<String>> futures = executor.invokeAll(IntStream.rangeClosed(1, 2)
+							.<Callable<String>>mapToObj(i -> () -> new MyHystrixCommand(MyAction.SUCCESS_DELAYED).execute()).collect(toList()));
+					for (Future<String> future : futures) {
+						future.get();
+					}
+					executor.shutdown();
+					assertThat(executor.awaitTermination(1, TimeUnit.SECONDS)).isTrue();
+				}).withCauseInstanceOf(HystrixRuntimeException.class);
+
+		ConfigurationManager.getConfigInstance().setProperty("hystrix.threadpool.default.coreSize", originalThreadpoolSize);
+	}
 }
